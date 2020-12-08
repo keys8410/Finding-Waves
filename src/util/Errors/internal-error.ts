aadc7a24431c5ef6bab14e415110e96dfbe54da9
keys8410 <yanalmeidagarcia@gmail.com>
2020-12-08 01:36:17 -0300

    feat: add generic class InternalError

---
+++ src/util/Errors/internal-error.ts
@@ -0,0 +1,12 @@
+// classe generica de erro
+export class InternalError extends Error {
+  constructor(
+    public message: string,
+    protected code: number = 500,
+    protected description?: string
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}

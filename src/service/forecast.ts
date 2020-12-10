a638c6c9f47edd87d463f4fb188cec624bdec1c6
keys8410 <yanalmeidagarcia@gmail.com>
2020-12-10 13:42:25 -0300

    feat: add Forecast

---
+++ src/service/forecast.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+}
